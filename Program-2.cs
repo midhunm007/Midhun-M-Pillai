d20b00c3ef4c8492151b90fb1db2c72d920461da
midhunm007 <136894205+midhunm007@users.noreply.github.com>
2025-08-08 21:56:29 +0530

    Program-2.cs

---
+++ Program-2.cs
@@ -0,0 +1,9 @@
+// language used C#
+
+Console.WriteLine("Enter the number of Limit :");
+int a = Convert.ToInt32(Console.ReadLine());
+
+for (int i = 0; i < a; i++)
+{
+    Console.Write(2 * i + 1 + ",");
+}
